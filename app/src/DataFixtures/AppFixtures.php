ca7cce480353ec6a2922729d7de8d911042e8eea
Danil Efimov <danil.efimov@aventusgroup.com>
2024-08-27 15:45:35 +0300

    Feat: add more fixtures

--- app/src/DataFixtures/AppFixtures.php
+++ app/src/DataFixtures/AppFixtures.php
@@ -12,14 +12,24 @@ class AppFixtures extends Fixture
   public function load(ObjectManager $manager)
   {
     $products = [
-      0 => ['id' => 1, 'name' => 'Tomato', 'description' => 'Tomato from Asia'],
-      1 => ['id' => 2, 'name' => 'Potato', 'description' => 'Potato from Belarus'],
-      2 => ['id' => 3, 'name' => 'Onion', 'description' => 'Onion from Japan'],
-      4 => ['id' => 4, 'name' => 'Apple', 'description' => 'Apple from China']
+      0 => ['id' => 1, 'name' => 'Tomato', 'description' => 'Tomato from Asia', 'image' => 'tomato.webp'],
+      1 => ['id' => 2, 'name' => 'Banana', 'description' => 'Banana from Ecvador', 'image' => 'banana.webp'],
+      2 => ['id' => 3, 'name' => 'Onion', 'description' => 'Onion from Japan', 'image' => 'onion.webp'],
+      4 => ['id' => 4, 'name' => 'Apple', 'description' => 'Apple from China', 'image' => 'apple.webp'],
+      5 => ['id' => 5, 'name' => 'Broccoli', 'description' => 'Broccoli from China', 'image' => 'broccoli.webp'],
+      6 => ['id' => 6, 'name' => 'Cabbage', 'description' => 'Broccoli from Russia', 'image' => 'cabbage.webp'],
+      7 => ['id' => 7, 'name' => 'Carrots', 'description' => 'Carrots from Belarus', 'image' => 'carrots.webp'],
+      8 => ['id' => 8, 'name' => 'Cucumbers', 'description' => 'Cucumbers from Belarus', 'image' => 'cucumbers.webp'],
+      9 => ['id' => 9, 'name' => 'Kiwi', 'description' => 'Kiwi from Africa', 'image' => 'kiwi.webp'],
+      10 => ['id' => 10, 'name' => 'Lemon', 'description' => 'Lemon from Africa', 'image' => 'lemon.webp'],
+      11 => ['id' => 11, 'name' => 'Lime', 'description' => 'Lime from Africa', 'image' => 'lime.webp'],
+      12 => ['id' => 12, 'name' => 'Melon', 'description' => 'Melon from Japan', 'image' => 'melon.webp'],
+      13 => ['id' => 13, 'name' => 'Nectarine', 'description' => 'Nectarine from Japan', 'image' => 'nectarine.webp'],
+      14 => ['id' => 14, 'name' => 'Peach', 'description' => 'Peach from Japan', 'image' => 'peach.webp'],
     ];
 
     foreach ($products as $id => $product) {
-      $this->createProduct($manager, $product['id'], $product['name'], $product['description']);
+      $this->createProduct($manager, $product['id'], $product['name'], $product['description'], $product['image']);
     }
 
     // Default user
@@ -61,14 +71,20 @@ class AppFixtures extends Fixture
   }
 
 
-  private function createProduct(ObjectManager $manager, int $id, string $name, string $description)
-  {
+  private function createProduct(
+    ObjectManager $manager,
+    int $id,
+    string $name,
+    string $description,
+    string $image
+  ) {
     $product = new Product();
 
     $product->setId($id);
     $product->setName($name);
     $product->setDescription($description);
-    $product->setPrice(mt_rand(5, 50) / mt_rand(1, 10));
+    $product->setImage($image);
+    $product->setPrice(round(mt_rand(2, 40) / mt_rand(1, 10), 2));
     $manager->persist($product);
   }
 }
